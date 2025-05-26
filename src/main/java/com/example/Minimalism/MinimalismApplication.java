9104df6b08c89cdb1a10c80b86e2bf51b2ac84ec
Amrit478 <Amritraj4786@gmail.com>
2025-05-26 17:40:11 -0400

    main folder

---
+++ src/main/java/com/example/Minimalism/MinimalismApplication.java
@@ -0,0 +1,14 @@
+package com.example.Minimalism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinimalismApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(MinimalismApplication.class, args);
+	}
+
+}
