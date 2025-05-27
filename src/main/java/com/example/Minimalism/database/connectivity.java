d9093792ed8a64277032ffe25c266124090e1adc
Amrit478 <Amritraj4786@gmail.com>
2025-05-27 08:26:28 -0400

    Make new branch and work there for this file

--- src/main/java/com/example/Minimalism/database/connectivity.java
+++ src/main/java/com/example/Minimalism/database/connectivity.java
@@ -1,4 +1,13 @@
 package com.example.Minimalism.database;
 
 public class connectivity {
+    /*In this i will make the quries of database and when i am able to do that
+    * succesfully than i will write a class for database connection and trying to
+    * change the port in application.properties and do more operations in that whatever
+    * i have written in the notepad file which follows after it*
+
+    I WILL CREATE NEW BRANCH FOR THIS AND THAN MERGE IT WHEN I AM DONE AND
+    * THIS WAY MY LEARNING WITH GITHUB IS GET BETTER AND I NEED TO ALSO WRITE
+    * A GITHUB ACTIONS FOLDER BEFORE THAT HAPPENS
+     */
 }
