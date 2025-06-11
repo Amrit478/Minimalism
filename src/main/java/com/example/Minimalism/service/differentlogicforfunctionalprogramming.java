629f1ee58d8f355a228ba0f433e275df7d4fb420
Amrit478 <Amritraj4786@gmail.com>
2025-06-11 15:00:24 -0400

    new

--- src/main/java/com/example/Minimalism/service/differentlogicforfunctionalprogramming.java
+++ src/main/java/com/example/Minimalism/service/differentlogicforfunctionalprogramming.java
@@ -1,2 +1,13 @@
-package com.example.Minimalism.service;public class differentlogicforfunctionalprogramming {
+package com.example.Minimalism.service;
+
+
+/*This is my service class where i will logic for the program and i will be using Repositories
+* and accesing database connectivity class and making changes into the database*/
+
+/*Will use constructor here for imperative programming and will be using the autowired annotation
+to implement other classes dependencies here as well
+ */
+public class differentlogicforfunctionalprogramming {
+
+
 }
--- src/main/java/com/example/Minimalism/service/logicstarter.java
+++ src/main/java/com/example/Minimalism/service/logicstarter.java
@@ -1,4 +1,9 @@
 package com.example.Minimalism.service;
 
+
+/*This is my service class where i will logic for the program and i will be using Repositories
+ * and accesing database connectivity class and making changes into the database*/
+
 public class logicstarter {
+    //Need to write functional programming here
 }
