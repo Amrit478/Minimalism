df68c894bd34d5b9f5dc1ad42dce6162f21983dd
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-08-07 14:28:04 -0400

    Create ENDPOINTSecurity.java

---
+++ src/main/java/com/example/Minimalism/Authorization/ENDPOINTSecurity.java
@@ -0,0 +1,3 @@
+/*By requiring a valid JWT with every request to protected endpoints,
+the system ensures that only authenticated and authorized users can access 
+sensitive resources and perform specific actions, thereby establishing secure endpoints for the system.*/
