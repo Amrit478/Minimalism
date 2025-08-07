75e2f8a9c18006a763c8f2fd3f026207ff69c3bb
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-08-07 15:29:50 -0400

    Create JwtUtil.java

---
+++ src/main/java/com/example/Minimalism/Security/JwtUtil.java
@@ -0,0 +1,4 @@
+/*Need to understand this class because this is copy paste from my computer..
+
+and this class using @Value annotation means it will get its properties from application.yml and */
+
