8e885eb8a0e4b9ccf1b24a8f48ef9c193e348d36
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-08-07 14:26:43 -0400

    Create Authorize.java

---
+++ src/main/java/com/example/Minimalism/Authorization/Authorize.java
@@ -0,0 +1,2 @@
+/*I will make addition or changes here when building authorization for user login and password and than i will assign the json web token with every request for the rest api so i know i have build safe endpoints for the system*/
+
