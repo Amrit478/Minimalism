168775ff187fdd282659cda133cd023ea263d4d5
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-08-12 19:02:27 -0400

    Update MinimalismApplication.java

--- src/main/java/com/example/Minimalism/MinimalismApplication.java
+++ src/main/java/com/example/Minimalism/MinimalismApplication.java
@@ -2,7 +2,7 @@ package com.example.Minimalism;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@EnableCaching
 @SpringBootApplication
 public class MinimalismApplication {
 
