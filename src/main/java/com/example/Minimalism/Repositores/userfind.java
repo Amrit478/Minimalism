4cfd21947710d195ec5cf6f8ecf80071163b5df8
AMRITRAJ S DHILLON <123419466+Amrit478@users.noreply.github.com>
2025-08-13 17:00:19 -0400

    Update userfind.java
    This is the new method which i have added for my repositories class

--- src/main/java/com/example/Minimalism/Repositores/userfind.java
+++ src/main/java/com/example/Minimalism/Repositores/userfind.java
@@ -3,6 +3,11 @@ package com.example.Minimalism.Repositores;
 import com.example.Minimalism.database.connectivity;
 
 @FunctionalInterface
-public interface userfind {
+public interface userfind extends JpaRespository <RefreshToken, Long id> {
     void userfind(connectivity user);
+
+    Optional<Refresh Token findByToken(String token); 
 }
+
+
+                    
